89a4058289a08c6c9f959047054e3f65f4eaab16
RazorFinger <pedro.lopes0203@gmail.com>
2019-04-26 12:26:38 -0300

    Big city, Camera, and Unity Chan

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float height, heightLook, distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform.position + Vector3.up * heightLook);
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + Vector3.up * height + player.transform.forward * distance, Time.smoothDeltaTime);
+    }
+}
---
+++ Assets/movePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed;
+    public float rotSpeed;
+    public Animator anim;
+    Vector3 moveAxis, turnAxis;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveAxis = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        turnAxis = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+
+        controller.SimpleMove(transform.TransformVector(moveAxis) * moveSpeed);
+        anim.SetFloat("Speed", controller.velocity.magnitude);
+        transform.Rotate(turnAxis * rotSpeed);
+    }
+}
