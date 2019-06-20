0480b832863dbf5c714b136287ec2634c273d9c8
devlog98 <dno1998@hotmail.com>
2019-06-20 12:38:11 -0300

    Menu and Credits scenes implemented!

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadScene(int i) {
+        SceneManager.LoadScene(i);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
