dd0fd49e05ed6495ec788c1a8edc096de09778ea
RazorFinger <pedro.lopes0203@gmail.com>
2019-06-21 16:38:14 -0300

    LOD implemented, Fog not visible far away, Swords reimported

---
+++ Assets/TurnOffFog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffFog : MonoBehaviour
+{
+    ParticleSystem fog;
+
+    // Update is called once per frame
+    void Start()
+    {
+        fog = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem.EmissionModule em = fog.emission;
+        em.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ParticleSystem.EmissionModule em = fog.emission;
+        em.enabled = false;
+    }
+}
