3fb4a758a7ca3f06feb38c393a270e073ef5d5e0
devlog98 <dno1998@hotmail.com>
2019-08-22 11:04:17 -0300

    load level parts separetely (not implemented in all map)

---
+++ Assets/Scripts/LoadAditiveScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAditiveScene : MonoBehaviour
+{
+    [SerializeField] string SceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(SceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.UnloadSceneAsync(SceneName);
+        }
+    }
+}
